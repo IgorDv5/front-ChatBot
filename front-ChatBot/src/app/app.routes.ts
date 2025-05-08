af4a791d12ab740a30c8ab0573b957ed8ebaed3a
IgorDv5 <igoraluno2005@gmail.com>
2025-05-07 22:04:45 -0300

    Correção de assets e criacao de componentes

--- front-ChatBot/src/app/app.routes.ts
+++ front-ChatBot/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FaleConoscoComponent } from './components/fale-conosco/fale-conosco.component';
+import { SobreNosComponent } from './components/sobre-nos/sobre-nos.component';
 
 export const routes: Routes = [
-    {
-        path: '',component: HeaderComponent
-    }
+
+      {
+        path: 'faleconosco',
+        component: FaleConoscoComponent
+      },
+      {
+        path: 'sobrenos',
+        component: SobreNosComponent
+      }
 ];
