33aaa36fb243ad6b68edfa209a8ca6e94e90be14
IgorDv5 <igoraluno2005@gmail.com>
2025-05-07 21:15:15 -0300

    Header Criado

--- front-ChatBot/src/app/components/header/header.component.ts
+++ front-ChatBot/src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  reloadCurrentRoute() {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
+    });
+  }
 }
